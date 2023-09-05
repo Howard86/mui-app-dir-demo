38ac50b35402465e0e7d9e0f5d9626dae8b1a5aa
Howard Tai <howard@howardism.dev>
2023-09-05 14:59:00 +0800

    refactor: add manual timeout for exit animation

--- src/app/modals/@modal/(..)products/[id]/page.tsx
+++ src/app/modals/@modal/(..)products/[id]/page.tsx
@@ -1,6 +1,8 @@
 'use client'
 
+import { useTheme } from '@mui/material/styles'
 import { useRouter } from 'next/navigation'
+import { useEffect, useRef, useState } from 'react'
 
 import ProductDialog from '@/components/ProductDialog'
 
@@ -13,8 +15,29 @@ export interface ProductModalSegmentProps {
 export default function ProductModalSegment({
   params,
 }: ProductModalSegmentProps) {
+  const theme = useTheme()
   const router = useRouter()
-  const onClose = () => router.back()
+  const timeoutRef = useRef<NodeJS.Timeout | undefined>()
+  const [open, setOpen] = useState(true)
+  const onClose = () => {
+    setOpen(false)
+    timeoutRef.current = setTimeout(
+      () => router.back(),
+      // Reference: packages/mui-material/src/styles/createTransitions.js
+      theme.transitions.duration.leavingScreen
+    )
+  }
 
-  return <ProductDialog id={parseInt(params.id, 10)} onClose={onClose} open />
+  useEffect(
+    () => () => {
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current)
+      }
+    },
+    []
+  )
+
+  return (
+    <ProductDialog id={parseInt(params.id, 10)} onClose={onClose} open={open} />
+  )
 }
